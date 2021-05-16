0df56c2dfef4e46dfcbbe6b1f2fa6f6f8317aaa7
Benjamin <benjamin.consterdine@gmail.com>
2021-05-16 17:50:03 +0100

    Implemented RamblersSearch class for A* search

---
+++ search4/RamblersSearch.java
@@ -0,0 +1,17 @@
+public class RamblersSearch extends Search {
+    private TerrainMap map; //map for traversal
+    private Coords goal; //Goal coordinates
+
+    public RamblersSearch(TerrainMap m, Coords g) {
+        map = m;
+        goal = g;
+    }
+
+    //Accesors
+    public TerrainMap getMap() {
+        return map;
+    }
+    public Coords getGoal() {
+        return goal;
+    }
+}
