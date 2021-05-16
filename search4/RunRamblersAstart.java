901ef570de73505c705896bf7f298ce6ab53e3a5
Benjamin <benjamin.consterdine@gmail.com>
2021-05-16 22:55:04 +0100

    Implemented class to run A* search

---
+++ search4/RunRamblersAstart.java
@@ -0,0 +1,14 @@
+public class RunRamblersAstart {
+    public static void main(String[] args) {
+        TerrainMap map = new TerrainMap("tmc.pgm");
+        Coords goal =  new Coords(8,3);
+        Coords start = new Coords(1,1);
+
+        RamblersSearch searcher = new RamblersSearch(map, goal);
+        SearchState initState = (SearchState) new RamblersState(start, 0, 0);
+
+        String res_astar = searcher.runSearch(initState, "AStar");
+        System.out.println(res_astar);
+    }
+}
+
